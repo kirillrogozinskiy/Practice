ada71425e73c779a91d5b39d3f1ec1ba9fc7f9f1
Кирилл Рогожинский <rogozhinskiy_kirill@outlook.com>
2025-04-01 10:30:56 +0300

    save

---
+++ Рогожинский_14/WpfApp/EmployeeWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfApp
+{
+    public partial class EmployeeWindow : Window
+    {
+        public MainWindow.Employee Employee { get; private set; }
+
+        public EmployeeWindow(MainWindow.Employee employee = null)
+        {
+            InitializeComponent();
+            Employee = employee ?? new MainWindow.Employee();
+            DataContext = this;
+        }
+
+        private void Ok_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(Employee.FullName) ||
+                string.IsNullOrWhiteSpace(Employee.Position))
+            {
+                MessageBox.Show("Заполните все поля!");
+                return;
+            }
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e) => DialogResult = false;
+    }
+}
